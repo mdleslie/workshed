c73f104ba36d545c271212f78c377927de4eeee1
mdleslie <mdleslie@gmail.com>
2023-09-21 13:39:33 -0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo
+
+echo -e "\e[1;32m Step 1: Updating packages. \e[0m"  
+sudo apt-get update
+
+flatpak update -y
+
+echo
+
+echo -e "\e[1;32m Step 2: Repairing Hudsucker Flatpacks. \e[0m"  
+sudo flatpak repair
+
+echo
+
+echo -e "\e[1;32m Step 3: Upgrading packages. \e[0m"  
+sudo apt-get upgrade -y  
+sudo apt-get dist upgrade -y  
+sudo apt-get update
+
+echo
+
+echo -e "\e[1;32m Step 4: Cleaning up. \e[0m"  
+sudo apt-get clean  
+sudo apt-get autoclean  
+sudo apt-get autoremove
+
+flatpak uninstall --unused -y
+
+echo -e "\e[1;32m Step 5: Script is finished running. Exiting now. Shop smart, Shop S-Mart! \e[0m"
+neofetch
+exit
