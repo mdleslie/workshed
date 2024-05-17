ebdf2f3065ece962f55fd4333c4a3d8cf1ca3b59
david <mdleslie@gmail.com>
2024-05-16 17:27:00 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,26 +17,26 @@ now=$(date)
 
 echo
 
-echo -e "\e[1;32m Step 1: Updating apt and flatpak packages. \e[0m"  
+echo -e "\e[1;32m Step 1: Updating apt and flatpak packages. Don't Mix Danger, Handle with Care. \e[0m"  
 sudo apt-get update
 
 flatpak update -y
 
 echo
 
-echo -e "\e[1;32m Step 2: Repairing Flatpacks. \e[0m"  
+echo -e "\e[1;32m Step 2: Repairing Flatpacks. Groovy. \e[0m"  
 sudo flatpak repair
 
 echo
 
-echo -e "\e[1;32m Step 3: Upgrading apt packages. \e[0m"  
+echo -e "\e[1;32m Step 3: Upgrading apt packages. And I do not hold to that. So no more runnin'. I aim to misbehave. \e[0m"  
 sudo apt-get upgrade -y  
 sudo apt-get dist upgrade -y  
 sudo apt-get update
 
 echo
 
-echo -e "\e[1;32m Step 4: Cleaning up apt and flatpak. \e[0m"  
+echo -e "\e[1;32m Step 4: Cleaning up apt and flatpak. Don't Panic. \e[0m"  
 sudo apt-get clean  
 sudo apt-get autoclean  
 sudo apt-get autoremove
