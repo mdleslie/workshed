c5f8275c005337f272116b6b6a9ed85b811d3a1d
david <mdleslie@gmail.com>
2024-08-20 12:07:25 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -72,7 +72,7 @@ if (( $(echo "$available_space_gb < 5" | bc -l) )); then
 fi
 
 # Prompt for sudo password early
-echo "This script requires sudo privileges. Please enter your password now."
+echo "This script requires sudo privileges. Enter password for $USER now."
 if sudo -v; then
     echo "Sudo access granted. Starting update process..."
 else
