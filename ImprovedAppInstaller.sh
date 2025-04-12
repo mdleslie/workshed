bee2ecae2ea86ebe3a720bcae5c0d29d595b7a94
david <mdleslie@gmail.com>
2025-04-12 04:48:39 +0000

    Update ImprovedAppInstaller.sh

--- ImprovedAppInstaller.sh
+++ ImprovedAppInstaller.sh
@@ -348,9 +348,9 @@ echo '########################################' | lolcat
 echo '########################################' | lolcat
 
 #Install yt-dlp
-log INFO "Installing Yt Dlp"
-display $GREEN "Installing YT DLP app"
-python3 -m pip install -U "yt-dlp[default]"
+#log INFO "Installing Yt Dlp"
+#display $GREEN "Installing YT DLP app"
+#python3 -m pip install -U "yt-dlp[default]"
 
 echo '########################################' | lolcat
 echo '########################################' | lolcat
