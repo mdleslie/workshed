d70efba4adca63c459252ada94a640ac63828658
david <mdleslie@gmail.com>
2025-07-01 07:35:28 -0700

    Update ImprovedAppInstaller.sh

--- ImprovedAppInstaller.sh
+++ ImprovedAppInstaller.sh
@@ -1,216 +1,108 @@
-# Pop OS/Ubuntu Setup Script - Maybe Slightly Improved Version
-
-
-
+# Pop OS/Ubuntu Setup Script - Cleaning up my mess version
+# well, trying to, anyway
 # Author: workshed
-
-
-# Description: Automated setup script for fresh Pop OS/Ubuntu Studio installations
-
-
-
-
+# Description: Automated setup script for fresh Pop OS/Ubuntu installations
 # Define the log file path
 
 log_file="/home/$USER/install_log.txt"
 
-
-
 # Function to log and display messages
 
 log_and_display() {
-
   timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-
   message="$timestamp: $1"
-
   echo "$message"
-
   echo "$message" >> "$log_file"
-
 }
-
-
 
 # List of .deb packages to install
 
 deb_packages=(
-
   "fortune-mod"
-
   "cowsay"
-
-
   "ubuntu-restricted-extras"
-
   "ffmpeg"
-
   "mpv"
-
   "mediainfo"
-
   "vlc"
-
   "libssl-dev"
-
   "libexpat1-dev"
-
   "libgl1-mesa-dev"
-
   "libgstreamer1.0-dev"
-
   "libgstreamer-plugins-base1.0-dev" 
-
   "libgstreamer-plugins-bad1.0-dev"  
-
   "gstreamer1.0-plugins-bad"   
-
   "gstreamer1.0-qt5"
-
   "gstreamer1.0-plugins-ugly"
-
   "gstreamer1.0-plugins-good"
-
   "gstreamer1.0-libav"
-
   "libavcodec-extra"
-
   "chromium-codecs-ffmpeg-extra" 
-
   "nfs-common"
-
   "cifs-utils"
-
   "gamemode"
-
   "lutris"
-
   "steam"
-
   "cpu-x"
-
   "python3"
-
-
   "python3-pip"
-
   "figlet"
-
   "fonts-inter"
-
   "mangohud"
-
   "ncdu"
-
-
   "pydf"  
-
-
   "ffmpegthumbnailer"
-
   "bind9-dnsutils"
-
   "inetutils-traceroute"
-
   "whois"
-
   "nmap"
-
   "btop"
-
   "cmake"
-
   "libcairo2-dev"
-
   "libx11-dev"
-
   "lv2-dev"
-
   "nasm"
-
-
-
 )
-
-
 
 # List of Flatpak applications to install
 
 flatpak_apps=(
-
   "org.libreoffice.LibreOffice"
-
   "net.cozic.joplin_desktop"
-
   "com.synology.SynologyDrive"
-
   "com.brave.Browser"
-
   "org.kde.kdenlive"
-
   "fr.handbrake.ghb"
-
   "com.obsproject.Studio"
-
   "io.missioncenter.MissionCenter"
-
   "org.telegram.desktop"
-
   "com.bitwarden.desktop"
-
   "io.github.aandrew_me.ytdn"
-
   "org.localsend.localsend_app"
-
   "io.github.shiftey.Desktop"
-
   "com.github.tchx84.Flatseal"
-
   "eu.betterbird.Betterbird"
-
   "net.davidotek.pupgui2"
-
   "com.vscodium.codium"
-
   "org.jdownloader.JDownloader"
-
   "com.github.qarmin.czkawka"
-
   "org.darktable.Darktable"
-
   "com.mattermost.Desktop"
-
   "com.google.Chrome"
-
   "io.github.flattool.Warehouse"
-
   "fm.reaper.Reaper"
-
   "org.guitarix.Guitarix"
-
   "com.discordapp.Discord"
-
   "org.kde.haruna"
-
   "com.github.IsmaelMartinez.teams_for_linux"
-
   "com.github.taiko2k.tauonmb"
-
   "com.makemkv.MakeMKV"
-
   "org.inkscape.Inkscape"
-
   "ar.com.tuxguitar.TuxGuitar"
-
   "com.rtosta.zapzap"
-
   "us.zoom.Zoom"
-
   "com.dropbox.Client"
-
 )
-
-
 
 # Array to store the names of installed .deb packages and Flatpak applications
 
@@ -218,435 +110,215 @@ installed_deb_packages=()
 
 installed_flatpak_apps=()
 
-
-
-
-
 # Check if lolcat is installed
 
 if ! command -v lolcat &> /dev/null; then
-
     log_and_display " lolcat is not installed. Installing lolcat."
-
     sleep 5s
-
     sudo apt update && sudo apt install -y lolcat
-
 fi
 
-
-
 echo '########################################' | lolcat
-
-
 
 # Define log files
 
 log_file="/home/$USER/install_log.txt"
-
 update_summary="/home/$USER/install_summary.txt"
-
-
 
 # Colors
 
 RED='\033[0;31m'
-
 GREEN='\033[0;32m'
-
 YELLOW='\033[1;33m'
-
 BLUE='\033[0;34m'
-
 NC='\033[0m' # No Color
-
-
 
 # Function to log messages
 
 log() {
-
     local level=$1
-
     local message=$2
-
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-
     echo "[$timestamp] [$level] $message" | tee -a "$log_file"
-
     logger -p user.$level "$message"
-
 }
-
-
 
 # Function to display colorful messages
 
 display() {
-
     local color=$1
-
     local message=$2
-
     echo -e "${color}$message${NC}" | tee -a "$log_file"
-
     echo -e "${color}$message${NC}" | lolcat
-
 }
-
-
 
 # Function to check if the output is a terminal
 
 lol() {
-
   if [ -t 1 ]; then
-
     "$@" | lolcat
-
   else
-
     "$@"
-
   fi
-
 }
-
-
 
 # Bind the function to the RETURN key
 
 bind 'RETURN: "\e[1~lol \e[4~\n"'
 
-
-
 # Error handling
 
 set -e
-
 trap 'log ERROR "An error occurred. Exit code: $?"' ERR
-
-
 
 # Cleanup function
 
 cleanup() {
 
     log INFO "Cleaning up..."
-
-
-
     # Remove any temporary files
-
     rm -f /tmp/install_script_*
-
-
-
     # Revert .bashrc if the script didn't complete successfully
-
     if [ -f ~/.bashrc.bak ] && [ "$script_completed" != "true" ]; then
-
         mv ~/.bashrc.bak ~/.bashrc
-
         log WARNING "Reverted .bashrc to original state."
-
     fi
-
-
-
     log INFO "Cleanup completed."
-
 }
-
-
 
 # Trap for cleanup
 
 trap cleanup EXIT
 
-
-
 # Variable to track script completion
 
 script_completed="false"
 
-
-
 # Function to cache sudo credentials
 
 cache_sudo() {
-
     sudo -v
-
     ( while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null ) &
-
 }
-
-
 
 # Start of script installation message
 
 log INFO "Starting installation script"
-
-
 display $GREEN "Lets go, it's showtime!"
-
-
 sleep 5s
-
 display $GREEN "This script will automate setting up a clean OS install."
-
 sleep 2s
-
 display $BLUE "Don't Mix Danger, Handle with Care!"
-
 sleep 2s
-
 display $RED "Don't Mix Danger, Handle with Care!"
-
 sleep 2s
-
 display $GREEN "Don't Mix Danger, Handle with Care!"
-
 sleep 5s
 
-
-
-
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
-
 
 # Cache sudo credentials
 
 log INFO "Caching sudo credentials"
-
 cache_sudo
-
-
 
 # Update and upgrade system
 
 log INFO "Updating and upgrading system"
-
 display $GREEN "Preparing system before installing new applications."
-
 sleep 2s
-
 sudo apt update
-
 sudo apt upgrade -y
 
-
-
 echo '########################################' | lolcat
-
-
 
 # Install Nala
 
 log INFO "Installing Nala"
-
 display $GREEN "Adding curl and installing Nala. Because it is better than apt."
-
 sleep 2s
-
 sudo apt install curl -y
-
 curl https://gitlab.com/volian/volian-archive/-/raw/main/install-nala.sh | bash
-
 sudo nala update
 
-
-
 echo '########################################' | lolcat
-
-
-
-# Remove LibreOffice 
-
-#This section is for Pop OS installs with old versions of Libreoffice.
-
-#Commenting out but leaving incase of future need.
-
-
-
-#log INFO "Removing LibreOffice"
-
-#display $GREEN "Removing the old packaged version of LibreOffice."
-
-#sleep 2s
-
-#sudo nala remove --purge -y "libreoffice*"
-
-#sudo nala clean 
-
-#sudo nala autoremove -y
-
-
-
 echo '########################################' | lolcat
-
-
-
-echo '########################################' | lolcat
-
-
 
 # Install FastFetch
-
 log INFO "Installing FastFetch"
-
 display $GREEN "Installing Fastfetch from the zhangsongcui repo."
-
 sleep 2s
-
 sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
-
 sudo nala update
-
 sudo nala install fastfetch -y
 
-
-
 echo '########################################' | lolcat
-
-
 
 # Preconfigure Microsoft fonts and libdvd-pkg
-
 log INFO "Preconfiguring Microsoft fonts and libdvd-pkg"
-
 echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | sudo debconf-set-selections
-
 export DEBIAN_FRONTEND=noninteractive
-
 sudo DEBIAN_FRONTEND=noninteractive apt -yq install libdvd-pkg
-
 sudo bash /usr/lib/libdvd-pkg/b-i_libdvdcss.sh
-
 unset DEBIAN_FRONTEND
 
-
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
-
 
 # Install .deb packages
-
 log INFO "Installing .deb packages"
-
 display $GREEN "Installing .deb packages."
-
 for package in "${deb_packages[@]}"; do
-
     if dpkg -l | grep -qw "$package"; then
-
         log INFO "$package is already installed, skipping."
-
     else
-
         log INFO "Installing $package"
-
         if sudo nala install -y "$package"; then
-
             installed_deb_packages+=("$package")
-
         else
-
             log ERROR "Failed to install $package"
-
         fi
-
     fi
-
 done
 
-
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
-
 
 # Install Flatpak applications
-
 log INFO "Installing Flatpak applications"
-
 display $GREEN "Installing Flatpak applications."
-
 if ! command -v flatpak &> /dev/null; then
-
     log INFO "Flatpak not installed, installing Flatpak"
-
     sudo nala install -y flatpak
-
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 fi
-
-
-
 for app in "${flatpak_apps[@]}"; do
-
     if flatpak list | grep -qw "$app"; then
-
         log INFO "$app is already installed, skipping."
-
     else
-
         log INFO "Installing $app"
-
         if flatpak install -y --noninteractive flathub "$app" &>> "$log_file"; then
-
             installed_flatpak_apps+=("$app")
-
             # The success message is now logged via the redirection above.
-
         else
-
             log ERROR "Failed to install $app"
-
         fi
-
     fi
-
 done
 
-
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
 echo '########################################' | lolcat
-
-
 
 # Generate installation report
 
