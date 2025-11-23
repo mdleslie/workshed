cf90340b1b865d185540a2d0c8a56845f98cbb2f
david <mdleslie@gmail.com>
2025-11-23 04:10:14 -0700

    Update WorkshedAppInstaller.sh

--- WorkshedAppInstaller.sh
+++ WorkshedAppInstaller.sh
@@ -163,7 +163,7 @@ echo '########################################' | lolcat
 display $GREEN "Gus, don't be William Zabka from Back to School."
 sleep 3s
 
-# FIX: Add Fastfetch PPA so it can be found on Pop!_OS
+# Add Fastfetch PPA so it can be found on Pop!_OS
 sudo add-apt-repository -y ppa:zhangsongcui333666/fastfetch
 
 
